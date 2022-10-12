c3e49656976ab529debc3fefb7256166085728f0
Benazir123 <benazirfathimanova@gmail.com>
2022-10-12 10:45:42 +0530

    carousel app oct 12

---
+++ my-carousel/src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Slider from "react-carousel-responsive";
+import "react-carousel-responsive/dist/styles.css";
+import "./styles.css";
+
+const App = () => (
+  <Slider>
+    <img
+      src="https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=630&h=354"
+      alt=""
+    />
+    <img
+      src="https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=630&h=354"
+      alt=""
+    />
+    <img
+      src="https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=630&h=354"
+      alt=""
+    />
+  </Slider>
+);
+
+export default App;
